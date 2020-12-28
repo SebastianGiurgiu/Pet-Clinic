523fafc106306b38d4b45f9658bd0cd467b7f1c0
Sebastian Giurgiu <sebastiangiurgiu1998@gmail.com>
2020-12-28 11:17:23 +0200

    first commit

---
+++ src/main/java/sebi/springframework/sebipetclinic/SebiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package sebi.springframework.sebipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebiPetClinicApplication.class, args);
+    }
+
+}
